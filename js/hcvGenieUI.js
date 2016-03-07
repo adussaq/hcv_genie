46b276744f342ca23685e99ce6d7b5a553619c9b
Alex <a.m.dussaq@gmail.com>
2016-03-06 18:24:13 -0600

    just remove the box on mobile test, could be a better test, but not a big deal to have it when we should not.

--- js/hcvGenieUI.js
+++ js/hcvGenieUI.js
@@ -383,7 +383,7 @@
 
         //test for mobile device
         if (
-            /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge\ |maemo|midp|mmp|netfront|opera\ m(ob|in)i|palm(\ os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows\ (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera\ Mini/i.test(navigator.userAgent)
         ) {
             dropRegion.parent().empty();
         }
